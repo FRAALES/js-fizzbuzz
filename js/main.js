a78cc3081a1886165c88c5b42605fe01d5fc5f9b
FRAALES <alessandrofranco1992@gmail.com>
2023-05-02 16:47:44 +0200

    inzio js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Seleziono contenitore
+
+const contenitore = document.getElementById("main-container");
+
+
+
+
+// cicliamo le operazioni di creazione e di output
+for(let i = 0; i <= 1000; i++) {
+    
+    // creiamo nuovo elemento
+    
+
+}
