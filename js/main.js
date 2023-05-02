2a624fe8ec8285d5ce089a896ae9659d0397107c
FRAALES <alessandrofranco1992@gmail.com>
2023-05-02 16:53:23 +0200

    impostato js ma non funziona

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ const contenitore = document.getElementById("main-container");
 for(let i = 0; i <= 1000; i++) {
     
     // creiamo nuovo elemento
-    
+    const quadrato = document.createElement('span');
 
+    // inseriamo il valore all'interno ell'elemento creato
+    quadrato.append(i);
+
+    // aggiungiamo al contenitore 'elemento creato
+    contenitore.append(quadrato)
 }
