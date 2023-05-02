d9f0ff2d970cc2bdba4342c2e4b0a80ac1bb0da1
FRAALES <alessandrofranco1992@gmail.com>
2023-05-02 17:51:12 +0200

    condizione aggiunta ma non risponde

--- js/main.js
+++ js/main.js
@@ -7,14 +7,24 @@ console.log(contenitore);
 
 
 // cicliamo le operazioni di creazione e di output
-for(let i = 0; i <= 1000; i++) {
+for(let i = 1; i <= 1000; i++) {
     
     // creiamo nuovo elemento
     const quadrato = document.createElement('span');
+    // Voglio aggiungere una classe ad ogni span 
+    quadrato.classList.add("square");
 
     // inseriamo il valore all'interno ell'elemento creato
     quadrato.append(i);
 
     // aggiungiamo al contenitore 'elemento creato
     contenitore.append(quadrato);
+
+
+
+    // aggiungiamo la condizione che se i è multiplo di 3, allora invece del numero vedrò scritto FIX
+        {
+            if (i / 3 === 3)
+            console.log("FIX");
+        }
 }
