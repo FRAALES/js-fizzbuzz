96d904dfc867dff688820f699c2d4103c24a641a
FRAALES <alessandrofranco1992@gmail.com>
2023-05-02 20:27:36 +0200

    Finito ma non cambia i colori

--- js/main.js
+++ js/main.js
@@ -7,24 +7,27 @@ console.log(contenitore);
 
 
 // cicliamo le operazioni di creazione e di output
-for(let i = 1; i <= 1000; i++) {
-    
+for(let i = 1; i <= 100; i++) {
+
     // creiamo nuovo elemento
     const quadrato = document.createElement('span');
     // Voglio aggiungere una classe ad ogni span 
     quadrato.classList.add("square");
 
-    // inseriamo il valore all'interno ell'elemento creato
-    quadrato.append(i);
+    // Imposto il testo del quadrato in base al valore corrente
+    if (i % 3 === 0 && i % 5 === 0) {
+        quadrato.textContent = "fixbux";
+        quadrato.classList.add("red");
+    } else if (i % 3 === 0) {
+        quadrato.textContent = "fix";
+        quadrato.classList.add("blue");
+    } else if (i % 5 === 0) {
+        quadrato.textContent = "bux";
+        quadrato.classList.add("yellow");
+    } else {
+        quadrato.textContent = i;
+    }
 
-    // aggiungiamo al contenitore 'elemento creato
+    // aggiungiamo al contenitore elemento creato
     contenitore.append(quadrato);
-
-
-
-    // aggiungiamo la condizione che se i è multiplo di 3, allora invece del numero vedrò scritto FIX
-        
-            if (i === 3) {
-                console.log("FIX");
-            }
 }
